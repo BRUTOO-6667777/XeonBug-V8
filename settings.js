6741b6a9dc982c81c505b386fe1f9ca84f337e8b
BRUTOO-6667777 <tfdfghffvcf@gmail.com>
2024-12-06 10:33:13 -0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,19 +10,19 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923444844060"
-global.ownername = "Qasim Ali"
-global.ytname = "YT: GlobalTechInfo"
+global.ownernomer = "917356710141"
+global.ownername = "BRUTO"
+global.ytname = "YT: KLZ BRUTO"
 global.socialm = "GitHub: GlobalTechInfo"
 global.location = "Pakistan, Karachi, Karachi"
 
-global.ownernumber = '923444844060'  //creator number
-global.ownername = 'Qasim Ali' //owner name
+global.ownernumber = '917356710141'  //creator number
+global.ownername = 'BRUTOZZZ' //owner name
 global.botname = 'XeonBug-V8' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
-global.author = 'GlobalTech\n\nContact: +923444844060'
+global.author = 'GlobalTech\n\nContact: +917356710141'
 
 //console view/theme
 global.themeemoji = '🪀'
